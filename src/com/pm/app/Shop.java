72ec91d28d2f0976e67d12ca06fde0db5907f2ee
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-03-09 14:01:24 -0100

    Product and Shop

---
+++ src/com/pm/app/Shop.java
@@ -0,0 +1,26 @@
+package com.pm.app;
+
+import com.pm.data.Product;
+
+import java.math.BigDecimal;
+
+/**
+ * @author Sangha
+ */
+public class Shop {
+
+    /**
+     *
+     * @params args the command line arguments
+     */
+    public static void main(String[] args){
+
+        Product p1 = new Product();
+        p1.setId(101);
+        p1.setName("Tea");
+        p1.setPrice(BigDecimal.valueOf(1.99));
+
+        System.out.println(p1.getId()+" "+p1.getName()+" "+p1.getPrice());
+
+    }
+}
---
+++ src/com/pm/data/Product.java
@@ -0,0 +1,35 @@
+package com.pm.data;
+
+import java.math.BigDecimal;
+
+public class Product {
+
+    private static final BigDecimal DISCOUNT_RATE = BigDecimal.valueOf(0.1);
+    private int id;
+    private String name;
+    private BigDecimal price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
