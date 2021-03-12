f6b298cd652dde4fe2d2f17e8e1edcfb84776394
Jailson Evora <6410930+jeevora@users.noreply.github.com>
2021-03-12 15:07:57 -0100

    Multiple Reviews for the Product

--- src/com/pm/app/Shop.java
+++ src/com/pm/app/Shop.java
@@ -23,6 +23,7 @@ public class Shop {
         Product p1 = pm.createProduct(101,"Tea",BigDecimal.valueOf(1.99), Rating.NOT_RATED);
         pm.printProductReport();
         p1 = pm.reviewProduct(p1, Rating.FOUR_STAR, "Nice hot cup of tea");
+        p1 = pm.reviewProduct(p1, Rating.FOUR_STAR, "Nice hot cup of tea");
         pm.printProductReport();
 
         /*Product p2 = pm.createProduct(102,"Coffee",BigDecimal.valueOf(1.99), Rating.FOUR_STAR);
